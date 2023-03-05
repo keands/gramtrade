5348a1515c8d79cc44cc3151f53e2f948be84220
KDS <kean.desouza@gmail.com>
2023-03-05 22:34:21 +0100

    correct error on regex pattern for sell limit

--- gramTradeConstants.py
+++ gramTradeConstants.py
@@ -2,7 +2,7 @@ buy_pattern = r"\b(buy|long|achat)\b"
 short_pattern = r"\b(sell|short|vente)\b"
 
 buy_limit_pattern = r"\b(buy limit)\b"
-sell_limit_pattern = r"\b(buy limit)\b"
+sell_limit_pattern = r"\b(sell limit)\b"
 
 entry_pattern = r"\b(entrée|entry|pe|actuel|@)\b"
 
